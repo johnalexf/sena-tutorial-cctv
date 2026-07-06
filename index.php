a3d4f1d869f13012032a9f6a1fd5b3a0d52d10f6
Johnalexf <johnalexfr@gmail.com>
2026-07-06 11:14:12 -0500

    feat(index): implementar grid de dos columnas para contenido y menu sticky

--- index.php
+++ index.php
@@ -1,17 +1,105 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Icono para pestañas en navegadores web-->
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico">
+
+    <title>Tutorial CCTV</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    
+    <!-- Google Fonts -->
+        <!-- Tipografías: Montserrat para títulos y Roboto para textos generales -->
+
+        <!-- Optimiza la conexión con los servidores de Google Fonts antes de cargar las fuentes -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <!-- Carga las familias tipográficas Montserrat y Roboto -->
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <!-- Fin Google Fonts -->
+
+    <!-- Estilos propios -->
+
+</head>
+
+<body class="container-xxl px-0 vh-100 d-flex flex-column bg-body-tertiary">
+
+    <!--Inicio del header -->
+    <header class="p-1">
+
+        <!--Inicio Contenedor principal con diseño navbar aun que no es directamente el nav tiene el boton que lleva al navbar  -->
+        <div class="navbar bg-white rounded-1 shadow-sm ">
+
+            <!--Inicio Contenedor secundario por defecto configuracion de boostrap -->
+            <div class="container-fluid d-flex align-items-center justify-content-center pt-1">
+
+                <!-- Titulo -->
+                <h1 class="text-primary fw-bold fs-3">
+                    Tutorial mantenimiento preventivo CCTV
+                </h1>
+
+            </div>
+            <!--Fin Contenedor secundario por defecto configuracion de boostrap -->
+
+        </div>
+        <!--Fin Contenedor principal con diseño navbar aun que no es directamente el nav tiene el boton que lleva al navbar  -->   
+
+    </header>
+    <!--Fin del header  -->
+
+
+    <!--Inicio del main esta estructurado para que ocupe todo el espacio disponible ademas permite hacer scroll manteniendo el espacio del header y del footer fijo en la pantalla -->
+    <main class="flex-grow-1 overflow-y-auto row px-3 m-0">
+
+        <!-- Inicio contenido del detalles de los pasos -->
+        <section class="col-9 py-3">
+            <h2 class="text-primary text-center fs-4">
+                Pasos para realizar un mantenimiento preventivo a un sistema de circuito cerrado de television (CCTV)
+            </h2>
+        </section>
+        <!-- Fin del contenido del detalle de los pasos -->
+
+        <!-- Inicio del menu de navegacion con la lista ordenada de los pasos -->
+         <!-- Se usa sticky-top para mantener fijo el menu de navegacion mientras se hace scroll en el contenido de los detalles -->
+        <nav class= "col-3 py-3 sticky-top border-secondary border-2 border-start">
+            <h2 class="text-primary text-center fs-4">
+                Menú de navegación
+            </h2>
+
+            <ol>
+                <li>
+                    paso 1
+                </li>
+            </ol>
+        </nav>
+        <!-- Fin del menu de navegacion con la lista ordenada de los pasos -->
+
+
+    </main>
+    <!-- Fin del main  -->
+
+
+    <!-- Inicio del footer -->
+    <footer class="p-1">
+        <div class="container-fluid bg-white border-top py-2 text-center text-muted rounded-1">
+            <small>&copy; 2026 John Alexander Foreo Rubio. <span class="d-block d-sm-inline">Tutorial mantenimiento preventivo CCTV.</span></small>
+        </div>
+    </footer>
+    <!-- Fin del footer -->
+
+
+
+
+    <!-- Bootstrap JS -->
+    <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    <!-- Iconify -->
+    <script src="./vendor/iconify/js/iconify-icon.min.js"></script>
+    
+</body>
 </html>
